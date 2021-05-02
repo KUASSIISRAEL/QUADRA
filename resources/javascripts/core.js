99e8399dcd63d3bc66945a3be12d6523f4f71eae
nga-office <nga.office19@gmail.com>
2021-05-02 19:14:39 +0000

    FINALIZE THE FRONT AND FIX BUG ON BACK AND FRONT

--- resources/javascripts/core.js
+++ resources/javascripts/core.js
@@ -6,6 +6,10 @@ new Vue({
       searchDatas: [],
       articleList: [],
       categoryList: [],
+      articleDesc: {
+        name: "",
+        description: "",
+      },
       config: {
         phaseOne: {
           display1: "",
@@ -33,33 +37,35 @@ new Vue({
             this.categoryList.push(item.categories)
           })
         })
-        .catch((error) => {
-          // console.log(error)
-        })
+        .catch((error) => {})
     },
     articleListResult(guid, category, number, image) {
-      this.config.phaseTwo.categoryTitle = category
+      this.config.phaseTwo.categoryTitle  = category
       this.config.phaseTwo.categoryNumber = number
-      this.config.phaseTwo.categoryImage = image
+      this.config.phaseTwo.categoryImage  = image
 
       // FECTCH THE DATA BY PROMISE
       axios.get(`/category/${guid}`)
         .then((response) => {
-          this.config.phaseOne.display2 = ""
-          this.config.phaseOne.display1 = "none"
-          this.config.phaseOne.display3 = "none"
+          this.config.phaseOne.display2    = ""
+          this.config.phaseOne.display1    = "none"
+          this.config.phaseOne.display3    = "none"
+          this.config.phaseOne.searchState = "none"
+
           response.data.forEach((item) => {
             this.articleList = item.article_list.articles
           })
         })
         .catch((error) => {
-          this.config.phaseOne.display2 = "none"
-          this.config.phaseOne.display1 = ""
-          this.config.phaseOne.display3 = "none"
-          console.log(error)
+          this.config.phaseOne.display2    = "none"
+          this.config.phaseOne.display1    = ""
+          this.config.phaseOne.display3    = "none"
+          this.config.phaseOne.searchState = "none"
         })
     },
     searchArticles() {
+      this.searchDatas = []
+
       axios.get(`/search?search=${this.search}`)
         .then((response) => {
           this.config.phaseOne.display2    = "none"
@@ -78,9 +84,25 @@ new Vue({
           this.config.phaseOne.display1    = ""
           this.config.phaseOne.display3    = "none"
           this.config.phaseOne.searchState = "none"
-
-          console.log(error)
         })
+    },
+    getArticleInfos(guid) {
+      axios.get(`/article/${guid}`)
+        .then((response) => {
+          this.config.phaseOne.display2    = "none"
+          this.config.phaseOne.display1    = "none"
+          this.config.phaseOne.display3    = ""
+          this.config.phaseOne.searchState = "none"
+          this.articleDesc.name            = response.data[0].articles.name
+          this.articleDesc.description     = response.data[0].articles.description
+        })
+        .catch((error) => {})
+    },
+    getTheDurationOfArticle(date) {
+      let time = new Date(date)
+      let now  = new Date()
+      let diff = now - time
+      return diff.toString()
     }
   }
 })
